b0b4889027451a2647a12f4f0d5550b3a4665a28
jenny <qkrwodms0201@gmail.com>
2020-11-27 17:57:06 +0900

    세션추가
    세션에 맞게 c태그 써서 다른화면 보이게할것

--- shop/web/src/main/java/shop/com/bbs/MemberController.java
+++ shop/web/src/main/java/shop/com/bbs/MemberController.java
@@ -1,6 +1,7 @@
 package shop.com.bbs;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +12,18 @@ import shop.com.dao.MemberDao;
 @Controller
 public class MemberController {
 
-	@Resource(name="memberDao")
+	@Resource(name = "memberDao")
 	private MemberDao dao;
-	
-	@RequestMapping(value="loginCheck.do")
-	public String loginCheckAction(MemberBean bean) {
-		System.out.println("Controller"+bean);
+
+	@RequestMapping(value = "loginCheck.do")
+	public String loginCheckAction(MemberBean bean, HttpSession session) {
+		// System.out.println("Controller" + bean);
 //		System.out.println(dao.loginCheck(bean));
-			if(dao.loginCheck(bean)) {
-				return "test";
-			}else return "SignIn2";
+		if (dao.loginCheck(bean)) {
+			// System.out.println(bean.getM_id());
+			session.setAttribute("id", bean.getM_id());
+			return "test";
+		} else
+			return "SignIn2";
 	}
 }
--- shop/web/src/main/java/shop/com/dao/MemberDao.java
+++ shop/web/src/main/java/shop/com/dao/MemberDao.java
@@ -6,19 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import shop.com.app.dto.MemberBean;
 
-
-public class MemberDao extends SqlSessionDaoSupport{
+public class MemberDao extends SqlSessionDaoSupport {
 
 	@Autowired
 	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
 		// TODO Auto-generated method stub
 		super.setSqlSessionFactory(sqlSessionFactory);
 	}
-	
+
 	public boolean loginCheck(MemberBean bean) {
 		String result = getSqlSession().selectOne("loginCheck", bean);
-		System.out.println("dao"+bean);
-		System.out.println("result"+result);
-		return (result==null)?false:true;
+		// System.out.println("dao"+bean);
+		// System.out.println("result"+result);
+		return (result == null) ? false : true;
 	}
 }
